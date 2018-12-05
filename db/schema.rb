06ac602b20640c55c2fbf7998d92de4e9f6d5a50
hwoj <hwojnicki41@gmail.com>
2018-12-05 15:33:30 -0500

    Created company model and migration and installed bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181011155638) do
+ActiveRecord::Schema.define(version: 20181205182233) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "companyname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
