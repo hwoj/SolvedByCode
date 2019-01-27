3ef6477859fdf48a2ada2e46ee36a01458767d36
hwoj <hwojnicki41@gmail.com>
2019-01-27 18:58:51 -0500

    fixed styling for company benefits

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -2,15 +2,15 @@ module CompaniesHelper
   def benefits(benefit)
     case benefit
     when "Dental care"
-      return '<i class="fas fa-tooth"></i> <span class="benefit"> Dental care</span>'
+      return '<i class="fas fa-tooth"></i> <span class="benefit" style="margin-left: 5px;> Dental care</span>'
     when "Health care"
-      return '<i class="fas fa-medkit"></i> <span class="benefit"> Health care</span>'
+      return '<i class="fas fa-medkit"></i> <span class="benefit" style="margin-left: 5px;> Health care</span>'
     when "Pension plans"
-      return '<span class="glyphicon glyphicon-usd"></span> <span class="benefit"> Pension plans</span>'
+      return '<span class="glyphicon glyphicon-usd"></span> <span class="benefit" style="margin-left: 5px;> Pension plans</span>'
     when "Work remotely"
-      return '<span class="glyphicon glyphicon-bed"></span> <span class="benefit"> Work remotely</span>'
+      return '<span class="glyphicon glyphicon-bed"></span> <span class="benefit" style="margin-left: 5px;"> Work remotely</span>'
     when "Daily free meals"
-      return '<i class="fas fa-utensils"></i> <span class="benefit"> Daily meals</span>'
+      return '<i class="fas fa-utensils"></i> <span class="benefit" style="margin-left: 5px;"> Daily meals</span>'
     when "Gym membership"
       return '<i class="fas fa-dumbbell"></i> <span class="benefit">Gym membership</span>'
     else
