18d2a483239c0ca6c98df6b91e9ccfd603536751
hwoj <hwojnicki41@gmail.com>
2019-01-27 16:46:52 -0500

    Created company show page and turned company size to radio instead of checkbox

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,18 +7,18 @@ class CompaniesController < ApplicationController
     @company = Company.new(company_params)
     if @company.save
       flash[:success] = "Thank you for signing up for SolvedByCode"
-      redirect_to jobs_path
+      redirect_to company_path(@company.id)
     else
       render 'new'
     end
   end
 
   def show
-
+    @company = Company.find(params[:id])
   end
   private
 
   def company_params
-    params.require(:company).permit(:companyname, :email, :password)
+    params.require(:company).permit(:companyname, :email, :password, :website, :benefits, :size, :location, :description, :industries, )
   end
 end
