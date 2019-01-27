3360c9897c75af970babcd9214951588e3b7726a
hwoj <hwojnicki41@gmail.com>
2019-01-27 18:52:35 -0500

    fixed some styling for company benefits

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -2,17 +2,17 @@ module CompaniesHelper
   def benefits(benefit)
     case benefit
     when "Dental care"
-      return '<i class="fas fa-tooth"></i> <span>Dental care</span>'
+      return '<i class="fas fa-tooth"></i> <span class="benefit"> Dental care</span>'
     when "Health care"
-      return '<i class="fas fa-medkit"></i> <span>Health care</span>'
+      return '<i class="fas fa-medkit"></i> <span class="benefit"> Health care</span>'
     when "Pension plans"
-      return '<span class="glyphicon glyphicon-usd"></span> <span>Pension plans</span>'
+      return '<span class="glyphicon glyphicon-usd"></span> <span class="benefit"> Pension plans</span>'
     when "Work remotely"
-      return '<span class="glyphicon glyphicon-bed"></span> <span>Work remotely</span>'
+      return '<span class="glyphicon glyphicon-bed"></span> <span class="benefit"> Work remotely</span>'
     when "Daily free meals"
-      return '<i class="fas fa-utensils"></i> <span>Daily meals</span>'
+      return '<i class="fas fa-utensils"></i> <span class="benefit"> Daily meals</span>'
     when "Gym membership"
-      return '<i class="fas fa-dumbbell"></i> <span>Daily meals</span>'
+      return '<i class="fas fa-dumbbell"></i> <span class="benefit">Gym membership</span>'
     else
       return benefit
     end
