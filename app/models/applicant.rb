4df9275c8ade4252ad24433af66973ca8169785b
hwoj <hwojnicki41@gmail.com>
2019-01-29 20:40:01 -0500

    Add password digest to applicants

---
+++ app/models/applicant.rb
@@ -0,0 +1,6 @@
+class Applicant < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  has_secure_password
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX, message: "Invalid email"}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130011841) do
+ActiveRecord::Schema.define(version: 20190130013804) do
 
   create_table "applicants", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "resume"
+    t.string "password_digest"
   end
 
   create_table "companies", force: :cascade do |t|
