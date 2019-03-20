ef3e454233d55e9918ec96954db4f9c5072283da
hwoj <hwojnicki41@gmail.com>
2019-03-20 12:20:58 -0400

    try to fix variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,10 +11,10 @@ require 'carrierwave/storage/fog'
       config.fog_provider = 'fog/aws'                        # required
       config.fog_credentials = {
           provider:              'AWS',                        # required
-          aws_access_key_id:     'AKIAIDE6DAUC74OJICVA',                        # required unless using use_iam_profile
-          aws_secret_access_key: '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',                        # required unless using use_iam_profile
+          aws_access_key_id:     ENV['AKIAIDE6DAUC74OJICVA'],                        # required unless using use_iam_profile
+          aws_secret_access_key: ENV['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'],                        # required unless using use_iam_profile
       }
-      config.fog_directory  = 'solved-by-code'                                      # required
+      config.fog_directory  = ENV['solved-by-code']                                      # required
     end
   end
 
