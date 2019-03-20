051b4676a1a32887b0361de29653036ee943d5c6
hwoj <hwojnicki41@gmail.com>
2019-03-20 11:48:46 -0400

    adding AWS credentials only to heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,8 @@ require 'carrierwave/storage/fog'
       config.fog_provider = 'fog/aws'                        # required
       config.fog_credentials = {
           provider:              'AWS',                        # required
-          aws_access_key_id:     'AKIAIM4OXIDZJS7WJ66Q',                        # required unless using use_iam_profile
-          aws_secret_access_key: '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',                        # required unless using use_iam_profile
+          aws_access_key_id:     'AKIAITUSXESPKGOX6EVA',                        # required unless using use_iam_profile
+          aws_secret_access_key: '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',                        # required unless using use_iam_profile
       }
       config.fog_directory  = 'solved-by-code'                                      # required
     end
