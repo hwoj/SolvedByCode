acc14a265ab3a6e8fcf9e6f28bc0d5e042ca36be
hwoj <hwojnicki41@gmail.com>
2019-03-19 21:36:56 -0400

    trying to fix problem with fog and heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-CarrierWave.configure do |config|
+require 'carrierwave/storage/fog'
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.storage = :file
@@ -8,14 +8,13 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     CarrierWave.configure do |config|
       config.storage = :fog
+      config.fog_provider = 'fog/aws'                        # required
+      config.fog_credentials = {
+          provider:              'AWS',                        # required
+          aws_access_key_id:     'AKIAIM4OXIDZJS7WJ66Q',                        # required unless using use_iam_profile
+          aws_secret_access_key: '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',                        # required unless using use_iam_profile
+      }
+      config.fog_directory  = 'solved-by-code'                                      # required
     end
   end
 
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAIM4OXIDZJS7WJ66Q',                        # required unless using use_iam_profile
-      aws_secret_access_key: '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',                        # required unless using use_iam_profile
-  }
-  config.fog_directory  = 'solved-by-code'                                      # required
-end
